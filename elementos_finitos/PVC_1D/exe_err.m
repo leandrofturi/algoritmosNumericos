387dc55a6b17c0ac65cdda3b68b4ffa9dd65e909
Leandro Furlam Turi <leandrofturi@gmail.com>
2020-09-19 14:11:16 -0300

    1D, ok

--- elementos_finitos/PVC_1D/exe_err.m
+++ elementos_finitos/PVC_1D/exe_err.m
@@ -1,42 +1,42 @@
-% Colocando de 0 a 1, não funciona nessa condição (k(0) = 0)
-a = 1;
-b = 2;
-
+a = 0;
+b = 1;
 x = [a, b];
-s = x.^3 - x + 1;
-s_l = 3*x.^2 - 1;
-
+s = x - (sinh(x)/sinh(1));
+s_l = 1 - (2*e/(-1+e^2))*cosh(x);
 alfa_a = 1;
 beta_a = 1;
 gama_a = s(1)+s_l(1);
-
 alfa_b = 0;
 beta_b = 1;
 gama_b = s(2);
 
 function y = k_x(x);
-  y = x;
-endfunction
-
-function y = c_x(x);
   y = ones(1, length(x));
 endfunction
 
+function y = c_x(x);
+  y = zeros(1, length(x));
+endfunction
+
 function y = b_x(x);
-  y = x.^2;
+  y = ones(1, length(x));
 endfunction
 
 function y = f_x(x);
-  y = x.^5 - x.^3 - 5*x.^2;
+  y = x;
 endfunction
 
+%[n_nos, x, h, K, F, k_el, c_el, b_el, f_el] = pre_processamento(a, b, N_el, @k_x, @c_x, @b_x, @f_x);
+%[K, F, u] = processamento(N_el, n_nos, x, K, F, alfa_a, beta_a, gama_a, alfa_b, beta_b, gama_b, k_el, c_el, b_el, f_el);
+%pos_processamento(linspace(a, b, n_nos), transpose(s), u);
+  
 elem = 2.^(3:8);
 j = 1;
 for i = elem
   [n_nos, x, h, K, F, k_el, c_el, b_el, f_el] = pre_processamento(a, b, i, @k_x, @c_x, @b_x, @f_x);
   [K, F, u] = processamento(i, n_nos, x, K, F, alfa_a, beta_a, gama_a, alfa_b, beta_b, gama_b, k_el, c_el, b_el, f_el);
   x = linspace(a, b, n_nos);
-  s = transpose(x.^3 - x + 1);
+  s = transpose(x - (sinh(x)/sinh(1)));
   er(j) = norm(abs(u-s), inf);
   j = j+1;
 endfor
--- elementos_finitos/PVC_1D/processamento.m
+++ elementos_finitos/PVC_1D/processamento.m
@@ -10,10 +10,11 @@ function [K, F, u] = processamento(N_el, n_nos, x, K, F, alfa_a, beta_a, gama_a,
     F(2) = F(2) - K(2, 1)*gama_a/beta_a;
     K(1, :) = K(:, 1) = 0;
     K(1, 1) = 1;
+    
   % Condição de contorno de Newmann ou mista em x = a
-  else
-    K(1, 1) = K(1, 1) - k_el(1)*beta_a/alfa_a;
-    F(1) = F(1) - k_el(1)*gama_a/alfa_a;
+else
+    K(1, 1) = K(1, 1) - k_el(1, 1)*beta_a/alfa_a;
+    F(1) = F(1) - k_el(1, 1)*gama_a/alfa_a;
   endif
 
   % Condição de contorno de Dirichlet em x = b
@@ -22,10 +23,11 @@ function [K, F, u] = processamento(N_el, n_nos, x, K, F, alfa_a, beta_a, gama_a,
     F(n_nos-1) = F(n_nos-1) - K(n_nos-1, n_nos)*gama_b/beta_b;
     K(n_nos, :) = K(:, n_nos) = 0;
     K(n_nos, n_nos) = 1;
+    
   % Condição de contorno de Newmann ou mista em x = b
   else
-    K(n_nos, n_nos) = K(n_nos, n_nos) + k_el(n_nos)*beta_b/alfa_b;
-    F(n_nos) = F(n_nos) + k_el(n_nos)*gama_b/alfa_b;
+    K(n_nos, n_nos) = K(n_nos, n_nos) + k_el(2, N_el)*beta_b/alfa_b;
+    F(n_nos) = F(n_nos) + k_el(2, N_el)*gama_b/alfa_b;
   endif
   
   u = K\F;
@@ -36,49 +38,49 @@ function K = K_el(x_el, k_el, c_el, b_el)
   K(1, 2) = - k_int(k_el, x_el) - c_int(c_el, x_el, x_el(2)) - b_int(b_el, x_el, x_el(2), x_el(1));
   K(2, 1) = - k_int(k_el, x_el) - c_int(c_el, x_el, x_el(1)) - b_int(b_el, x_el, x_el(1), x_el(2));
   K(2, 2) = k_int(k_el, x_el) + c_int(c_el, x_el, x_el(1)) + b_int(b_el, x_el, x_el(1), x_el(1));
+  
+  function y = k_int(k_el, x_el)
+    h = x_el(2) - x_el(1);
+    x1 = x_el(1);
+    x2 = x_el(2);
+    k1 = k_el(1);
+    k2 = k_el(2);
+    y = (1/h^3)*( ( (x2^2/2)*(-k1 + k2) + x2*(k1*x2 - k2*x1) ) -
+                  ( (x1^2/2)*(-k1 + k2) + x1*(k1*x2 - k2*x1) ) );
+  endfunction
+
+  function y = c_int(c_el, x_el, A)
+    h = x_el(2) - x_el(1);
+    x1 = x_el(1);
+    x2 = x_el(2);
+    c1 = c_el(1);
+    c2 = c_el(2);
+    y = (1/h^3)*( ( (x2^3/3)*(-c1 + c2) + (x2^2/2)*(c1*x2 - c2*x1 + A*(c1 - c2)) + x2*A*(-c1*x2 + c2*x1) ) - 
+                  ( (x1^3/3)*(-c1 + c2) + (x1^2/2)*(c1*x2 - c2*x1 + A*(c1 - c2)) + x1*A*(-c1*x2 + c2*x1) ) );
+  endfunction
+
+  function y = b_int(b_el, x_el, A, B)
+    h = x_el(2) - x_el(1);
+    x1 = x_el(1);
+    x2 = x_el(2);
+    b1 = b_el(1);
+    b2 = b_el(2);
+    y = (1/h^3)*( ( (x2^4/4)*(-b1 + b2) + (x2^3/3)*(b1*x2 - b2*x1 + (A+B)*(b1 - b2)) + (x2^2/2)*((A+B)*(-b1*x2 + b2*x1) + A*B*(-b1 + b2)) + x2*A*B*(b1*x2 - b2*x1) ) - 
+                  ( (x1^4/4)*(-b1 + b2) + (x1^3/3)*(b1*x2 - b2*x1 + (A+B)*(b1 - b2)) + (x1^2/2)*((A+B)*(-b1*x2 + b2*x1) + A*B*(-b1 + b2)) + x1*A*B*(b1*x2 - b2*x1) ) );
+  endfunction
 endfunction
 
 function F = F_el(x_el, f_el)
   F(1, 1) = - f_int(f_el, x_el, x_el(2));
   F(2, 1) = f_int(f_el, x_el, x_el(1));
-endfunction
-
-function y = k_int(k_el, x_el)
-  h = x_el(2) - x_el(1);
-  x1 = x_el(1);
-  x2 = x_el(2);
-  k1 = k_el(2);
-  k2 = k_el(2);
-  y = (1/h^3)*( ( (x2^2/2)*(-k1 + k2) + x2*(k1*x2 - k2*x1) ) -
-                ( (x1^2/2)*(-k1 + k2) + x1*(k1*x2 - k2*x1) ) );
-endfunction
-
-function y = c_int(c_el, x_el, A)
-  h = x_el(2) - x_el(1);
-  x1 = x_el(1);
-  x2 = x_el(2);
-  c1 = c_el(1);
-  c2 = c_el(2);
-  y = (1/h^3)*( ( (x2^3/3 - (x2^2/2)*A)*(-c1 + c2) + (x2^2/2 - A*x2)*(c1*x2 - c2*x1) ) - 
-                ( (x1^3/3 - (x1^2/2)*A)*(-c1 + c2) + (x1^2/2 - A*x1)*(c1*x2 - c2*x1) ) );
-endfunction
-
-function y = b_int(b_el, x_el, A, B)
-  h = x_el(2) - x_el(1);
-  x1 = x_el(1);
-  x2 = x_el(2);
-  b1 = b_el(1);
-  b2 = b_el(2);
-  y = (1/h^3)*( ( (x2^4/4 - (x2^3/3)*(A+B) + (x2^2/2)*A*B)*(-b1 + b2) + (x2^3/3 - (x2^2/2)*(A+B) + x2*A*B)*(b1*x2 - b2*x1) ) - 
-                ( (x1^4/4 - (x1^3/3)*(A+B) + (x1^2/2)*A*B)*(-b1 + b2) + (x1^3/3 - (x1^2/2)*(A+B) + x1*A*B)*(b1*x2 - b2*x1) ) );
-endfunction
-
-function y = f_int(f_el, x_el, A)
-  h = x_el(2) - x_el(1);
-  x1 = x_el(1);
-  x2 = x_el(2);
-  f1 = f_el(1);
-  f2 = f_el(2);
-  y = (1/h^2)*( ( (x2^3/3 - (x2^2/2)*A)*(-f1 + f2) + (x2^2/2 - A*x2)*(f1*x2 - f2*x1) ) - 
-                ( (x1^3/3 - (x1^2/2)*A)*(-f1 + f2) + (x1^2/2 - A*x1)*(f1*x2 - f2*x1) ) );
+  
+  function y = f_int(f_el, x_el, A)
+    h = x_el(2) - x_el(1);
+    x1 = x_el(1);
+    x2 = x_el(2);
+    f1 = f_el(1);
+    f2 = f_el(2);
+    y = (1/h^2)*( ( (x2^3/3)*(-f1 + f2) + (x2^2/2)*(f1*x2 - f2*x1 + A*(f1 - f2)) + x2*A*(-f1*x2 + f2*x1) ) - 
+                  ( (x1^3/3)*(-f1 + f2) + (x1^2/2)*(f1*x2 - f2*x1 + A*(f1 - f2)) + x1*A*(-f1*x2 + f2*x1) ) );
+  endfunction
 endfunction
